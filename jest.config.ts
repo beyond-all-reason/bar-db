ea66b22dec78d56cce89f021b241caa36b6c545e
Jazcash <jaspercashmore@gmail.com>
2021-01-31 23:23:49 +0000

    github ci deploy

---
+++ jest.config.ts
@@ -0,0 +1,8 @@
+import type {Config} from '@jest/types';
+
+const config: Config.InitialOptions = {
+  verbose: false,
+  coveragePathIgnorePatterns: ["working-files"],
+  modulePathIgnorePatterns: ["working-files"],
+};
+export default config;
