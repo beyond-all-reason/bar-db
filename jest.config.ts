c3e07e50f529e6a75db21bf6cf83a07b4ca910b5
Jazcash <jaspercashmore@gmail.com>
2021-01-31 23:36:51 +0000

    lint

--- jest.config.ts
+++ jest.config.ts
@@ -1,8 +1,8 @@
-import type {Config} from '@jest/types';
+import type {Config} from "@jest/types";
 
 const config: Config.InitialOptions = {
-  verbose: false,
-  coveragePathIgnorePatterns: ["working-files"],
-  modulePathIgnorePatterns: ["working-files"],
+    verbose: false,
+    coveragePathIgnorePatterns: ["working-files"],
+    modulePathIgnorePatterns: ["working-files"],
 };
 export default config;
