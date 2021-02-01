e2c46db6b7ab07b1c9bd3378e651ac71acc6826f
Jazcash <jaspercashmore@gmail.com>
2021-02-01 20:27:55 +0000

    don't deploy via github actions...

--- src/bar-db-cli.ts
+++ src/bar-db-cli.ts
@@ -8,7 +8,6 @@ const args = yargs(process.argv.slice(2))
     .option("pgusername", { type: "string", description: "Run with verbose logging", default: "postgres" })
     .option("pgpassword", { type: "string", description: "Run with verbose logging", default: "test" })
     .option("diagram", { type: "boolean", description: "Create an SVG diagram of the database schema", default: false })
-    .option("apiport", { type: "number", description: "Port API server should serve on", default: 3005 })
     .argv;
 
 import { BARDB } from "./bar-db";
@@ -19,8 +18,7 @@ const barDb = new BARDB({
     dbUsername: args.pgusername,
     dbPassword: args.pgpassword,
     verbose: args.verbose,
-    createSchemaDiagram: args.diagram,
-    apiPort: args.apiport
+    createSchemaDiagram: args.diagram
 });
 
 barDb.init();
